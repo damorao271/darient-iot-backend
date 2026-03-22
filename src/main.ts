1642bdd99f0a9291e01ad58bec48ce875079d922
damorao271 <damorao27@gmail.com>
2026-03-21 22:50:12 -0400

    Prevent swagger from running in production

--- src/main.ts
+++ src/main.ts
@@ -6,17 +6,19 @@ import { addResponseSchemas } from './common/swagger/schemas';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
-    .setTitle('Darient IoT Backend Test')
-    .setDescription(
-      'Swagger documentation for the Darient IoT Backend test Daniel Morao Nishimura. All responses use a standardized format: success (with data) or error (with details).',
-    )
-    .setVersion('1.0')
-    .addApiKey({ type: 'apiKey', name: 'x-api-key', in: 'header' }, 'api-key')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  // addResponseSchemas(document);
-  SwaggerModule.setup('api', app, document);
+  if (process.env.NODE_ENV !== 'production') {
+    const config = new DocumentBuilder()
+      .setTitle('Darient IoT Backend Test')
+      .setDescription(
+        'Swagger documentation for the Darient IoT Backend test Daniel Morao Nishimura. All responses use a standardized format: success (with data) or error (with details).',
+      )
+      .setVersion('1.0')
+      .addApiKey({ type: 'apiKey', name: 'x-api-key', in: 'header' }, 'api-key')
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    addResponseSchemas(document);
+    SwaggerModule.setup('api', app, document);
+  }
 
   await app.listen(process.env.PORT ?? 3000);
 }
