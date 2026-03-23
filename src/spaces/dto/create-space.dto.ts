6f9aac0708dccad834fb11dc39ba182474bda418
damorao271 <damorao27@gmail.com>
2026-03-22 23:33:43 -0400

    Get spaces by place

--- src/spaces/dto/create-space.dto.ts
+++ src/spaces/dto/create-space.dto.ts
@@ -4,7 +4,7 @@ import { z } from 'zod';
 const CreateSpaceSchema = z.object({
   placeId: z.string().cuid(),
   name: z.string().min(1).max(100),
-  reference: z.string().max(500).optional(),
+  reference: z.string().max(25).optional(),
   capacity: z.number().int().positive(),
   description: z.string().max(500).optional(),
 });
--- src/spaces/dto/update-space.dto.ts
+++ src/spaces/dto/update-space.dto.ts
@@ -4,7 +4,7 @@ import { z } from 'zod';
 const UpdateSpaceSchema = z.object({
   placeId: z.string().cuid().optional(),
   name: z.string().min(1).max(100).optional(),
-  reference: z.string().max(500).optional(),
+  reference: z.string().max(25).optional(),
   capacity: z.number().int().positive().optional(),
   description: z.string().max(500).optional(),
 });
